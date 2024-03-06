ddfcece9bbcfb57ab6efb0f56cad218141e4a2f1
princetomar <madm47157@gmail.com>
2024-03-07 01:10:12 +0530

    Navigation fixed

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/shared/Header";
+import React from "react";
+
+const AddTransformationTypePage = () => {
+  return (
+    <Header title="Transformation Title" subtitle="Transformation Subtitle" />
+  );
+};
+
+export default AddTransformationTypePage;
